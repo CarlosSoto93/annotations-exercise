f3a098643f366c39785ed63c1d2048575d3dbd68
Carlos <c.soto93@hotmail.com>
2017-05-24 09:35:28 -0500

    Updated east fix

--- src/Author.java
+++ src/Author.java
@@ -5,7 +5,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings({"unchecked", "deprication"})
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
